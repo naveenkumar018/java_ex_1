a7a0e1ed675f4075d9e7b3641d4c2f468de0f86b
NAVEENKUMAR <naveenkumarrajendran3@gmail.com>
2021-09-28 12:29:52 +0530

    first commit

---
+++ Example_For_Loop.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Example_For_Loop {
+	public static void main(String args[]){
+		System.out.println("Even numbers between 1 and 21 ");
+		for(int i=0;i<=20;i+=2)
+			System.out.println(i);
+	}
+
+}
---
+++ Sum_of_N_Numbers.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Sum_of_N_Numbers {
+	public static void main(String args[]){
+	int a[]={12,13,14,15,16};
+	int sum=0;
+	for(int i=0;i<a.length;++i){
+		sum+=a[i];
+	}
+	System.out.println(sum);
+	}
+}
